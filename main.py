001db9bbee205fad2cb4e08616dc678314e21367
Onur Baha <91025153+OnurBahaOkumus@users.noreply.github.com>
2023-12-18 16:27:43 +0300

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 def generated_password(length: int = 15):
   alphabet = string.ascii_letters + string.digits + string.punctuation
-  password = random.choice(alphabet) for i in range(length)
+  password = ''.join(random.choice(alphabet) for i in range(length))
   return password
 
 password = generated_password()
